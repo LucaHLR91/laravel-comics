f948b4897bcd5af27ecb7fe7e358c5316b30da3e
LucaHLR <mastrantonio.luca.91@icloud.com>
2021-10-27 17:54:19 +0200

    Main cards

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -3,34 +3,44 @@
 @section('title', 'Comics')
 
 @section('comics_main')
-
-
-<div class="main-button">
-    <button>Load More</button>
-</div>
-
-<div class="main-links">
-    <div class="container container_main_links">
-        <div class="link">
-            <img src="{{ asset('img/buy-comics-digital-comics.png') }}" alt="buy comics">
-            <span>digital comics</span>
-        </div>
-        <div class="link">
-            <img src="{{ asset('img/buy-comics-merchandise.png') }}" alt="buy merchandise">
-            <span>dc merchandise</span>
-        </div>
-        <div class="link">
-            <img src="{{ asset('img/buy-comics-subscriptions.png') }}" alt="subscribtions">
-            <span>subscriptions</span>
-        </div>
-        <div class="link">
-            <img src="{{ asset('img/buy-comics-shop-locator.png') }}" alt="store locator">
-            <span>comic shop locator</span>
-        </div>
-        <div class="link">
-            <img src="{{ asset('img/buy-dc-power-visa.svg') }}" alt="dc power visa">
-            <span>dc power visa</span>
-        </div>  
+<main>
+    <div class="main-comics">
+        @foreach ($comics as $comicbook)
+            <div class="card">
+                <img src="{{ $comicbook['thumb'] }}" alt="comicbook cover">
+                <h3>{{ $comicbook['title'] }}</h3>
+            </div>
+        @endforeach
     </div>
-</div>
+    
+    <div class="main-button">
+        <button>Load More</button>
+    </div>
+    
+    <div class="main-links">
+        <div class="container container_main_links">
+            <div class="link">
+                <img src="{{ asset('img/buy-comics-digital-comics.png') }}" alt="buy comics">
+                <span>digital comics</span>
+            </div>
+            <div class="link">
+                <img src="{{ asset('img/buy-comics-merchandise.png') }}" alt="buy merchandise">
+                <span>dc merchandise</span>
+            </div>
+            <div class="link">
+                <img src="{{ asset('img/buy-comics-subscriptions.png') }}" alt="subscribtions">
+                <span>subscriptions</span>
+            </div>
+            <div class="link">
+                <img src="{{ asset('img/buy-comics-shop-locator.png') }}" alt="store locator">
+                <span>comic shop locator</span>
+            </div>
+            <div class="link">
+                <img src="{{ asset('img/buy-dc-power-visa.svg') }}" alt="dc power visa">
+                <span>dc power visa</span>
+            </div>  
+        </div>
+    </div>
+
+</main>
 @endsection
