8d44955ae1f7d2b9ec838ca0302830d3e6f37b82
LucaHLR <mastrantonio.luca.91@icloud.com>
2021-10-27 17:05:35 +0200

    footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,67 @@
+<footer>
+    <div class="footer-top">
+        <div class="container container_footer_top">
+            <div class="footer-nav">
+                <h3 class="footer-nav-title">DC Comics</h3>
+                <ul>
+                    <li><a href="#">Characters</a></li>
+                    <li><a href="#">Comics</a></li>
+                    <li><a href="#">Movies</a></li>
+                    <li><a href="#">TV</a></li>
+                    <li><a href="#">Games</a></li>
+                    <li><a href="#">Videos</a></li>
+                    <li><a href="#">News</a></li>
+                </ul>
+
+                <h3 class="footer-nav-title">Shop</h3>
+                <ul>
+                    <li><a href="#">Shop DC</a></li>
+                    <li><a href="#">Shop DC Collectibles</a></li>
+                </ul>
+            </div>
+        <div class="footer-nav">
+            <h3 class="footer-nav-title">DC</h3>
+            <ul>
+                <li><a href="#">Terms Of Use</a></li>
+                <li><a href="#">Privacy policy(New)</a></li>
+                <li><a href="#">Ad Choices</a></li>
+                <li><a href="#">Advertising</a></li>
+                <li><a href="#">Jobs</a></li>
+                <li><a href="#">Subscription</a></li>
+                <li><a href="#">Talent Workspace</a></li>
+                <li><a href="#">CPSC Certificates</a></li>
+                <li><a href="#">Ratings</a></li>
+                <li><a href="#">Shop Help</a></li>
+                <li><a href="#">Contact US</a></li>
+            </ul>
+        </div>
+        <div class="footer-nav">
+            <h3 class="footer-nav-title">Sites</h3>
+            <ul>
+                <li><a href="#">DC</a></li>
+                <li><a href="#">MAD Magazine</a></li>
+                <li><a href="#">DC Kids</a></li>
+                <li><a href="#">DC Universe</a></li>
+                <li><a href="#">DC Power Visa</a></li>
+            </ul>
+        </div>
+        </div>
+    </div>
+    <div class="footer-bottom">
+        <div class="container container_footer_bottom">
+        <div class="sign-up-btn">
+            <button>Sign-up Now!</button>
+        </div>
+        <div class="social-media-links">
+            <ul>
+                <li><a href="#">Follow Us</a></li>
+                <li><a href="#"></a><img src="../img/footer-facebook.png" alt=""></li>
+                <li><a href="#"></a><img src="../img/footer-twitter.png" alt=""></li>
+                <li><a href="#"></a><img src="../img/footer-youtube.png" alt=""></li>
+                <li><a href="#"></a><img src="../img/footer-pinterest.png" alt=""></li>
+                <li><a href="#"></a><img src="../img/footer-periscope.png" alt=""></li>
+            </ul>
+        </div>
+        </div>
+    </div>
+</footer>
